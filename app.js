35e276ebdb64614f78d069d3a3c2c7b758a9145c
Victoria <victoria.rezbastos@gmail.com>
2021-09-14 14:42:00 -0300

    Lógica - Livros recomendados

--- app.js
+++ app.js
@@ -15,9 +15,15 @@ console.log('Bem vindo ao gerenciador de livros');
 const searchBook = inputTerminal.question('Você gostaria de buscar livros por categoria? (S/N)').toLocaleUpperCase()
 if(searchBook == 'S'){
     const booksByGenre = inputTerminal.question('Por favor, escolha uma das categorias disponíveis: biographies, education, fiction, mental health.').toLocaleLowerCase()
-    const booksSelected = books.filter((books) => books.genre === booksByGenre )
-    console.table(booksSelected)
+    const booksSelectedByGenre = books.filter((books) => books.genre === booksByGenre )
+    console.table(booksSelectedByGenre)
 }else{
+    const searchBooksWeLove = question('Você gostaria de conferir a lista de livros recomendados? (S/N)').toLocaleUpperCase()
+    if( searchBooksWeLove === 'S'){
+        const booksWeLove = books.filter((books) => books.booksWeLove === true)
+        console.table(booksWeLove)
+    }else{
     console.log('Esses são todos os livros listados:')
     console.table(books)
+    }
 }
