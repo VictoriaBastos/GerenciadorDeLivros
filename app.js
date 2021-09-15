5f953b9ef86c8bf55199b5de1b5c1b75e18b04b1
Victoria <victoria.rezbastos@gmail.com>
2021-09-15 17:11:16 -0300

    Gerenciador de Livros vs1.0

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
-// npm init
-// npm install readline-sync
-
-//  Importando books do módulo database.js
 const books = require('./database.js');
 
-// Importando readline-sync para receber inputs no terminal
 const inputTerminal = require('readline-sync');
 const question = inputTerminal.question
 
-// SISTEMA GERENCIADOR DE LIVROS
+console.log('´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`');
+console.log('                            Gerenciador de Livros');
+console.log('´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`');
 
-console.log('Bem vindo ao gerenciador de livros');
 
-const searchBook = inputTerminal.question('Você gostaria de buscar livros por categoria? (S/N)').toLocaleUpperCase()
-if(searchBook == 'S'){
-    const booksByGenre = inputTerminal.question('Por favor, escolha uma das categorias disponíveis: biographies, education, fiction, mental health.').toLocaleLowerCase()
+let searchBook = question('Voce gostaria de buscar livros por categoria? (S/N)').toLocaleUpperCase()
+if(searchBook === 'S'){
+    const booksByGenre = question('Por favor, escolha uma das categorias disponiveis:\n- Biografias \n- Educacao \n- Ficcao \n- Autoconhecimento.\n\t').toLocaleLowerCase() 
     const booksSelectedByGenre = books.filter((books) => books.genre === booksByGenre )
+    if(booksSelectedByGenre == false ){
+        console.log(`A categoria ${booksByGenre} não está listada.`)
+        searchBook = 'N'
+    }else{ 
+    console.log(`Confira abaixo os livros da categoria ${booksByGenre}!`)
     console.table(booksSelectedByGenre)
-}else{
-    const searchBooksWeLove = question('Você gostaria de conferir a lista de livros recomendados? (S/N)').toLocaleUpperCase()
-    if( searchBooksWeLove === 'S'){
-        const booksWeLove = books.filter((books) => books.booksWeLove === true)
-        console.table(booksWeLove)
-    }else{
-        const searchMyWishList = question('Você gostaria de ver os livros da minha wish list? (S/N)').toLocaleUpperCase()
-        if(searchMyWishList === 'S'){
-            const booksWishList = books.filter((books) => books.finished === false)
-            console.table(booksWishList)
-        }else{
-            console.log('Esses são todos os livros disponíveis:')
-            books.sort(( a, b ) => a.pages - b.pages)
-            console.table(books)
-        }
     }
 }
+
+const searchBooksWeLove = question('\nVoce gostaria de conferir a lista de livros recomendados? (S/N)').toLocaleUpperCase()
+if( searchBooksWeLove === 'S'){
+    const booksWeLove = books.filter((books) => books.booksWeLove === true)
+    console.log('\Confira abaixo os livros recomendados!')
+    console.table(booksWeLove)
+}
+
+const searchMyWishList = question('\nVoce gostaria de conferir os livros da Wish List? (S/N)').toLocaleUpperCase()
+if(searchMyWishList === 'S'){
+    const booksWishList = books.filter((books) => books.finished === false)
+    console.log('\Confira abaixo os livros da Wish List!')
+    console.table(booksWishList)
+}
+
+if(searchBook != 'S' && searchBooksWeLove != 'S' && searchMyWishList != 'S'){
+    console.log('\nEsses sao todos os livros listados:')
+    books.sort(( a, b ) => a.pages - b.pages)
+    console.table(books)
+}
+console.log('\n´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`');
+console.log('                              Victoria Bastos'                                 );
+console.log('                                {reprograma}                                  ');
+console.log('´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`´`');
